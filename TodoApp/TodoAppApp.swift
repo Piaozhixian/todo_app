c56f7383dfefd2014603ba423040ea3494253a43
Piao <pakushiken@gmail.com>
2022-02-14 21:06:59 +0900

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Zhixian Piao on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
